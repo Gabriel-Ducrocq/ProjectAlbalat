f5e6c571f631340c38a57b1adf918aa1d40edb5e
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2026-06-24 18:43:59 +0200

    first commit

---
+++ albalat/scripts/books_to_paragraphs.py
@@ -0,0 +1,359 @@
+"""
+This script processes the "common-pile/project_gutenberg" Hugging Face dataset.
+Several steps are involved:
+1/ Download the Gutenberg project catalog.
+2/ Keep only the books in English
+3/ Keep only the books belonging to specific bookshelves (genres)
+4/ Use this list to keep the corresponding texts in the "common-pile/project_gutenberg" dataset
+5/ Chunk each book into paragraphs:
+    1. Normalize text
+    2. Break text into paragraphs.
+    3. Remove dialogues, footnotes, transcriber's notes.
+    4. Keeps track of the chapterization
+
+The output of this script is Hugging Face dataset in Parquet format where each row is a
+paragraph and colums are:
+- text_id: identifier of the text
+- paragraph: actual text
+- span: starting and ending character numbers of the paragraph in the original text
+- chapter: chapter number in which the paragraph occurs if relevent.
+"""
+
+import re
+import pandas as pd
+from tqdm import tqdm
+from pathlib import Path
+from datasets import load_dataset
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+GENRES_KEPT = {
+    "novels",
+    "britishliterature",
+    "adventure",
+    "americanliterature",
+    "children",
+    "youngadultreading",
+    "sciencefiction",
+    "biographies",
+    "historyamerican",
+    "historymodern(1750+)",
+    "fantasy",
+    "shortstories",
+    "historicalnovels",
+    "historybritish",
+    "travelwriting",
+    "historywarfare",
+    "historyother",
+    "folklore",
+    "mythology",
+    "legends",
+    "historyeuropean",
+    "romance",
+    "crime",
+    "thrillersandmystery",
+    "historyearlymodern(c.14501750)",
+    "frenchliterature",
+    "classicsofliterature",
+}
+
+BOOKSHELVES_REGEX = re.compile(r"[,;&]")
+PATTERN_CHAPTER = re.compile(
+    r"^(?i:CHAPTER) [IVXLCDM]+|^[IVXLCDM]+$|^[A-Z ]{2,}\.{0,1}$|^No. [0-9]+--[A-Z ]+|^Part [IVXLCDM]+.{0,1}$|^Chapter [A-Z][a-zA-Z]{2,13}|^\s*(\*\s*){3,}\*\s*$"
+)
+
+PATTERN_FOOTNOTE = re.compile(r"\[footnote.+")
+PATTERN_TRANSCRIBER = re.compile(r"\[transcriber.+")
+PATTERN_ILLUSTRATION = re.compile(r"\[illustration")
+PATTERN_3_MORE_SPACE = re.compile(r"\s+")
+
+
+def get_bookshelves(bookshelf):
+    """
+    Gets all the bookshelves for a book
+    :param bookshelf: string of potentially multiple bookshelves.
+    :return: list of bookshelves.
+    """
+    if bookshelf is None:
+        return None
+
+    splitted_result = re.split(
+        BOOKSHELVES_REGEX,
+        bookshelf.replace("Category: ", "").lower().replace("-", "").replace(" ", ""),
+    )
+    return splitted_result if isinstance(splitted_result, list) else [splitted_result]
+
+
+def keep_on_genre_exclusive(bookshelves):
+    """
+    Whether the books that have all their bookshelves included in GENRES_KEPT.
+    :param bookshelves: list of bookshelves
+    :return: boolean, True if all bookshelves are to keep.
+    """
+    return set(bookshelves).issubset(GENRES_KEPT)
+
+
+def extract_metadata_hf(hf_dataset):
+    """
+    Extracts the metadata and the text ids from the Hugging Face dataset
+    :param hf_dataset: Hugging Face dataset woth columns ['license', 'language', 'url', 'title'].
+    :return: a pandas dataframe of the mentionned columns + 1 column ['id']
+    """
+    tuples_metadata_id = []
+    print("Extracting the metadata from the common_pile dataset:")
+    for t in tqdm(hf_dataset):
+        row = t["metadata"]
+        row.update({"text_id": t["id"]})
+        tuples_metadata_id.append(row)
+
+    metadata_id = pd.DataFrame.from_dict(tuples_metadata_id)
+    return metadata_id
+
+
+def create_metadata_table(merged_catalog_meta_data, language_list=["en"]):
+    """
+    Takes the catalog of the Gutenberg project and the common_pile/gutenberg-project dataset
+    and creates a metadata table, containing data on which we will filter the dataset:
+    - Language
+    - Bookshelves (genre)
+    - Is there any missing value
+
+
+    We also keep the id of the work so we know what to filter in and out in the common_pile dataset
+    :param catalog: pandas dataframe
+    :param metadata_common_pile: pandas dataframe with the metadata of the common pile dataset
+    :return: merged tables, filtered.
+    """
+    merged_catalog_meta_data = merged_catalog_meta_data.loc[
+        merged_catalog_meta_data.Language.isin(language_list)
+    ]
+    merged_catalog_meta_data = merged_catalog_meta_data[
+        ~merged_catalog_meta_data.Bookshelves.isna()
+    ]
+    mask_genres = merged_catalog_meta_data.Bookshelves_bag_of_words.apply(
+        keep_on_genre_exclusive
+    )
+    kept_books = merged_catalog_meta_data[mask_genres]
+    return kept_books
+
+
+def compute_inclusion_condition(words, p):
+    """
+    Computes whether to keep the paragraph or not. Discards if:
+    - Less or equal than 3 words
+    - Is entire upper characters
+    - Is a footnote
+    - Is a transcriber's note
+    - Is an illustration
+
+    Note that paragraph that are just chapter titles are discarded and will not be embedded, even though we keep track
+    of them.
+    arguments:
+    :param words: list words in the paragraph
+    :param p: full paragraph.
+    :return: boolean, False if discarded, True otherwise.
+    """
+    p = p.strip()
+    more_three_words = len(words) > 3
+    not_dialog = len(words) > 10 or (
+        """\"""" not in p and """“""" not in p and """\'""" not in p
+    )
+    not_all_upper = not p.isupper()
+    p = p.lower()
+    is_not_footnote = re.search(PATTERN_FOOTNOTE, p) is None
+    is_not_transcribers = re.search(PATTERN_TRANSCRIBER, p) is None
+    is_not_illustration = re.search(PATTERN_ILLUSTRATION, p) is None
+    return (
+        more_three_words
+        and not_dialog
+        and not_all_upper
+        and is_not_footnote
+        and is_not_illustration
+        and is_not_transcribers
+    )
+
+
+def determine_chapterization(text):
+    """
+    Determines whether this book uses a chapterization scheme or not at all.
+    :param text: content of the paragraph
+    :return: boolean, True of there is chapterization, False otherwise.
+    """
+    no_chapterization = False
+    if re.search(PATTERN_CHAPTER, text) is None:
+        no_chapterization = True
+
+    return no_chapterization
+
+
+def normalize_book(book: str) -> str:
+    """
+    Normalizes a text by replacing special characters introduced by Project Gutenberg, replacing the paragraphs spacing
+    (i.e \n\n) by a specific character: ||||, replacing the line skips introduced by Project Gutenberg with a mere
+    spacing, and removing the escaped apostrophe.
+    :param book: content of the book
+    :return: normalized content of the book
+    """
+    paragraphs = (
+        book.replace("\r\n", "\n")
+        .replace("\xa0", "")
+        .replace("\n\n", "||||")
+        .replace("\n", " ")
+        .replace("'", "'")
+    )
+    paragraphs = re.sub(PATTERN_3_MORE_SPACE, " ", paragraphs)
+    return paragraphs
+
+
+def pipeline(book, text_id):
+    """
+    This function chunks a book into paragraphs, keeps track of the chapterization, span in the origina text
+    and the text id.
+    parameters:
+    book -- str, content of the book, unprocessed.
+    text_id -- integer, id of the text.
+    return:
+    list of dict, paragraphs in the book.
+    """
+    paragraphs = normalize_book(book)
+    no_chapterization = determine_chapterization(paragraphs)
+    all_samples = {"paragraphs": [], "text_ids": [], "spans": [], "chapters": []}
+    paragraphs_chunks = paragraphs.split("||||")
+    chapter = None
+    for paragraph_num, p in enumerate(paragraphs_chunks):
+        p = p.strip()
+        match = re.search(PATTERN_CHAPTER, p)
+        if match is not None:
+            chapter = match.group()
+            continue
+
+        if chapter is not None or no_chapterization:
+            words = p.split()
+            condition_inclusion = compute_inclusion_condition(words, p)
+            if condition_inclusion:
+                result_search = re.search(re.escape(p), paragraphs)
+                assert result_search is not None, (
+                    f"Cannot find paragraph number {paragraph_num} in original text id {text_id}: {p}"
+                )
+                all_samples["paragraphs"].append(p)
+                all_samples["text_ids"].append(text_id)
+                all_samples["spans"].append(result_search.span())
+                all_samples["chapters"].append(chapter)
+
+    assert len(set([len(v) for k, v in all_samples.items()])) == 1, (
+        "All attributes must have the same length."
+    )
+    return all_samples
+
+
+def map_pipeline(sample_batch):
+    """
+    Wrapper function apply pipeline to the rows of the Hugging Face dataset.
+    """
+    batch = {"paragraphs": [], "text_ids": [], "spans": [], "chapters": []}
+    for num_sample in range(len(sample_batch["id"])):
+        all_samples = pipeline(
+            sample_batch["text"][num_sample], int(sample_batch["id"][num_sample])
+        )
+        batch["paragraphs"] += all_samples["paragraphs"]
+        batch["text_ids"] += all_samples["text_ids"]
+        batch["spans"] += all_samples["spans"]
+        batch["chapters"] += all_samples["chapters"]
+
+    return batch
+
+
+def merge_and_select_columns(catalog, metadata_hf):
+    """
+    Merges the two dataframes and select only the colums ["Language","text_id", "Bookshelves"]
+    :param catalog: dataframe made of the catalog of the Gutenberg project
+    :param metadata_hf: dataframe made of the metadata present in the common_pile/gutenberg-project dataset.
+    :return: a merged dataframe
+    """
+    merged_catalog_meta_data = pd.merge(
+        catalog, metadata_hf, how="inner", left_on="Text#", right_on="text_id"
+    )
+    merged_catalog_meta_data = merged_catalog_meta_data[
+        ["Language", "text_id", "Bookshelves"]
+    ]
+    return merged_catalog_meta_data
+
+
+def save_dataframe(df):
+    """
+    Saves a dataframe at ../data/interim/metadata_ids.csv
+    :param df: dataframe of metadata from common_pile/gutenberg-project
+    :return: None
+    """
+    df.to_csv(BASE_DIR / "data" / "interim" / "metadata_ids.csv")
+
+
+def filter_hf_dataset(hf_dataset, values):
+    """
+    Filters the HF dataset to keep only the given book ids.
+    :param hf_dataset: Hugging Face dataset of common_pile/gutenberg-project
+    :param values: values of books id to keep
+    :return: a hf_dataset filtered to keep only the correct indexes.
+    """
+    return hf_dataset.filter(lambda x: eval(x["id"]) in values)
+
+
+def map_hf_dataset(hf_dataset_filtered, batched=True, num_proc=8, batch_size=128):
+    """
+    Applies the map_pipeline function to the filtered HF dataset to split the books into paragraphs. Each paragraphs
+    have 5 fields:
+    - paragraphs: the content of the paragraph.
+    - text_ids: the book id.
+    - spans: a tuple (start character number, end character number) know where to find this paragraph in the book.
+    - chapters: the chapter number it belongs to, as a string (e.g "CHAPTER IV").
+
+    :param hf_dataset_filtered: common_pile/gutenberg-project filtered dataset.
+    :param batched: whether to use the batch version of map.
+    :param num_proc: number of processors to use.
+    :param batch_size: the number of books in each batch.
+    :return: Hugging face dataset with
+    """
+    return hf_dataset_filtered.map(
+        map_pipeline,
+        batched=batched,
+        num_proc=num_proc,
+        batch_size=batch_size,
+        remove_columns=["id", "text", "source", "added", "metadata"],
+    )
+
+
+def books_to_paragraphs(
+    streaming=False, languages=["en"], batched=True, num_proc=8, batch_size=128
+):
+    """
+
+    :return:
+    """
+    common_pile_dataset = load_dataset(
+        "common-pile/project_gutenberg", split="train", streaming=streaming
+    )
+    catalog = pd.read_csv(
+        "https://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv.gz",
+        compression="gzip",
+    )
+    metadata_hf = extract_metadata_hf(common_pile_dataset)
+    save_dataframe(metadata_hf)
+    merged_catalog_meta_data = merge_and_select_columns(catalog, metadata_hf)
+    merged_catalog_meta_data.Bookshelves_bag_of_words = (
+        merged_catalog_meta_data.Bookshelves.apply(get_bookshelves)
+    )
+    kept_books = create_metadata_table(
+        merged_catalog_meta_data, language_list=languages
+    )
+    common_pile_dataset_filtered = filter_hf_dataset(
+        common_pile_dataset, kept_books.text_id.values
+    )
+    processed_pararaphs = map_hf_dataset(
+        common_pile_dataset_filtered, batched, num_proc, batch_size
+    )
+    processed_pararaphs.to_parquet("paragraphs.parquet")
+
+
+if __name__ == "__main__":
+    pass
---
+++ albalat/tests/test_books_to_paragraphs.py
@@ -0,0 +1,322 @@
+import os
+import pandas as pd
+from pathlib import Path
+from datasets import Dataset
+from albalat.scripts.books_to_paragraphs import (
+    extract_metadata_hf,
+    get_bookshelves,
+    keep_on_genre_exclusive,
+    compute_inclusion_condition,
+    determine_chapterization,
+    normalize_book,
+    pipeline,
+    map_pipeline,
+    create_metadata_table,
+    merge_and_select_columns,
+    save_dataframe,
+    filter_hf_dataset,
+    map_hf_dataset,
+)
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class TestBooksToParagraphs:
+    def test_metadata_extractor(self):
+        fake_data = {
+            "id": [0, 1, 2, 3],
+            "metadata": [
+                {"language": "en", "url": "test"},
+                {"language": "fr", "url": "test"},
+            ]
+            * 2,
+            "extra": [0, 3, 5, 2],
+        }
+        fake_hf = Dataset.from_dict(fake_data)
+        fake_pd = pd.DataFrame.from_dict(
+            {"language": ["en", "fr"] * 2, "url": ["test"] * 4, "text_id": [0, 1, 2, 3]}
+        )
+        extracted_fake_pd = extract_metadata_hf(fake_hf)
+        assert set(extracted_fake_pd.columns.values) == set(fake_pd.columns.values), (
+            "Wrong column extraction."
+        )
+        assert extracted_fake_pd.equals(fake_pd), (
+            "Extracted and expected dataframes differ."
+        )
+
+    def test_get_bookshelves(self):
+        test_bookshelf = "Category: test1&TeSt2 -; test3, test4"
+        bookshelves = get_bookshelves(test_bookshelf)
+        assert bookshelves == [
+            "test1",
+            "test2",
+            "test3",
+            "test4",
+        ], f"""Booksehlves not correctly extracted:
+                                                                            {bookshelves} != ["test1", "test2", "test3", "test4"]"""
+
+    def test_keep_on_genre(self):
+        assert keep_on_genre_exclusive(
+            ["americanliterature", "children", "youngadultreading"]
+        ), (
+            "Should keep the bookshelves:['americanliterature','children','youngadultreading']"
+        )
+
+        assert not keep_on_genre_exclusive(
+            ["americanliterature", "test", "youngadultreading"]
+        ), (
+            "Should not keep the bookshelves:['americanliterature','test','youngadultreading']"
+        )
+
+    def test_compute_inclusion_condition(self):
+        p = "[Footnote this is a footnote"
+        words = p.split()
+        assert not compute_inclusion_condition(words, p), (
+            "Footnote should be discarded."
+        )
+        p = "[Transcriber: this is a transcriber note"
+        words = p.split()
+        assert not compute_inclusion_condition(words, p), (
+            "Transcriber's note should be discarded."
+        )
+        p = "[IllUSTration: this is an illustration"
+        words = p.split()
+        assert not compute_inclusion_condition(words, p), (
+            "Illustration should be discarded."
+        )
+
+        p = "'Hehehe' the crowd was laughing"
+        words = p.split()
+        assert not compute_inclusion_condition(words, p), "Dialog should be discarded."
+
+        p = "Said Mary"
+        words = p.split()
+        assert not compute_inclusion_condition(words, p), (
+            "Less than 3 words should be discarded."
+        )
+
+        p = "The bird is flying, the computer computing, and the music playing."
+        words = p.split()
+        assert compute_inclusion_condition(words, p), (
+            f"This sentence '{p}' should be kept."
+        )
+
+    def test_determine_chapterization(self):
+        text = [
+            " CHAPTER VI. pjojm",
+            " IX",
+            "THE CAT.",
+            "No. 10--THE CAT",
+            " Part IX.",
+            " Chapter Test",
+            "  *   *   *   *   *",
+        ]
+        for t in text:
+            assert determine_chapterization(t.strip()) is False, (
+                f"This text {t} should trigger chapterization"
+            )
+
+    def test_normalize_book(self):
+        assert (
+            normalize_book("Test \r\n sentence \xa0 is \n\n different\n from ' another")
+            == """Test sentence is |||| different from ' another"""
+        )
+
+    def test_pipeline(self):
+        book = """
+        Test book \n\n This is \n\n CHAPTER I \n\n this is a fake book \n that mimicks a gutenberg project \r\n by \xa0 adding
+        many special \' characters \n\n and creating a fake paragraph. \n\n CHAPTER II \n\n This is a new \n\n chapter in two
+        paragraphs
+        """
+        expected_paragraphs = {
+            "paragraphs": [
+                """this is a fake book that mimicks a gutenberg project by adding many special ' characters""",
+                "and creating a fake paragraph.",
+                "This is a new",
+                "chapter in two paragraphs",
+            ],
+            "text_ids": [1, 1, 1, 1],
+            "spans": [(44, 132), (138, 168), (190, 203), (209, 234)],
+            "chapters": ["CHAPTER I", "CHAPTER I", "CHAPTER II", "CHAPTER II"],
+        }
+        paragraphs = pipeline(book, 1)
+        assert expected_paragraphs["paragraphs"] == paragraphs["paragraphs"], (
+            "Paragraphs differ"
+        )
+        assert expected_paragraphs["spans"] == paragraphs["spans"], "Spans differ"
+        assert expected_paragraphs["text_ids"] == paragraphs["text_ids"], (
+            "text_id differ"
+        )
+        assert expected_paragraphs["chapters"] == paragraphs["chapters"], (
+            "chapter differ"
+        )
+        assert (
+            expected_paragraphs == paragraphs
+        ), f"""The text is not correctly cut into paragraphs:\n\n
+        cut: {paragraphs}\n\n
+        expected paragraphs: {expected_paragraphs}"""
+
+    def test_map_pipeline(self):
+        book = """
+        Test book \n\n This is \n\n CHAPTER I \n\n this is a fake book \n that mimicks a gutenberg project \r\n by \xa0 adding
+        many special \' characters \n\n and creating a fake paragraph. \n\n CHAPTER II \n\n This is a new \n\n chapter in two
+        paragraphs
+        """
+        sample_batch = {"text": [book, book], "id": [1, 2]}
+
+        expected_paragraphs = {
+            "paragraphs": [
+                """this is a fake book that mimicks a gutenberg project by adding many special ' characters""",
+                "and creating a fake paragraph.",
+                "This is a new",
+                "chapter in two paragraphs",
+            ]
+            * 2,
+            "text_ids": [1, 1, 1, 1, 2, 2, 2, 2],
+            "spans": [(44, 132), (138, 168), (190, 203), (209, 234)] * 2,
+            "chapters": ["CHAPTER I", "CHAPTER I", "CHAPTER II", "CHAPTER II"] * 2,
+        }
+        all_paragraphs = map_pipeline(sample_batch)
+        assert expected_paragraphs["paragraphs"] == all_paragraphs["paragraphs"], (
+            "Paragraphs differ"
+        )
+        assert expected_paragraphs["spans"] == all_paragraphs["spans"], "Spans differ"
+        assert expected_paragraphs["text_ids"] == all_paragraphs["text_ids"], (
+            "text_id differ"
+        )
+        assert expected_paragraphs["chapters"] == all_paragraphs["chapters"], (
+            "chapter differ"
+        )
+        assert (
+            expected_paragraphs == all_paragraphs
+        ), f"""The text is not correctly cut into paragraphs:\n\n
+        cut: {all_paragraphs}\n\n
+        expected paragraphs: {expected_paragraphs}"""
+
+    def test_merge_and_select(self):
+        catalog = pd.DataFrame.from_dict(
+            {
+                "Text#": [1, 2, 3, 4],
+                "Type": ["test"] * 4,
+                "Issued": ["test"] * 4,
+                "Title": ["title"] * 4,
+                "Language": ["fr", "en"] * 2,
+                "Authors": ["author"] * 4,
+                "Subjects": ["subject"] * 4,
+                "LoCC": ["test"] * 4,
+                "Bookshelves": ["bookshelf"] * 4,
+            }
+        )
+        fake_pd = pd.DataFrame.from_dict(
+            {"language": ["fr", "en"] * 2, "url": ["test"] * 4, "text_id": [0, 1, 2, 3]}
+        )
+        expected_table = pd.DataFrame.from_dict(
+            {
+                "Language": ["fr", "en", "fr"],
+                "text_id": [1, 2, 3],
+                "Bookshelves": ["bookshelf"] * 3,
+            }
+        )
+        merged_table = merge_and_select_columns(catalog, fake_pd)
+        assert expected_table.equals(
+            merged_table
+        ), f"""Merging and selecting columns failed for catalog and metadata."
+        expected: {expected_table}\n\n
+        obtained: {merged_table}  
+        """
+
+    def test_create_metadata_table(self):
+        merged_catalog_meta_data = pd.DataFrame.from_dict(
+            {
+                "Language": ["en", "fr", "it"] * 2,
+                "Bookshelves": [
+                    "novels & british literature",
+                    "novels; folklore",
+                    "biographies",
+                    "science",
+                    "poetry",
+                    "economics",
+                ],
+            }
+        )
+        merged_catalog_meta_data["Bookshelves_bag_of_words"] = (
+            merged_catalog_meta_data.Bookshelves.apply(get_bookshelves)
+        )
+        expected = merged_catalog_meta_data.loc[[0, 1]]
+        kept_books = create_metadata_table(
+            merged_catalog_meta_data, language_list=["en", "fr"]
+        )
+        assert kept_books.equals(expected), (
+            f"The books kept table: {kept_books}\n\n does not match the expected table {expected}"
+        )
+
+    def test_save_dataframe(self):
+        df = pd.DataFrame.from_dict({"test": [1] * 4})
+        save_dataframe(df)
+        has_raised = False
+        try:
+            df_loaded = pd.read_csv(
+                BASE_DIR / "data" / "interim" / "metadata_ids.csv", index_col=0
+            )
+        except FileNotFoundError:
+            has_raised = True
+        assert not has_raised, (
+            "The file '../data/interim/metadata_ids.csv' does not exists"
+        )
+        assert df_loaded.equals(df), (
+            "The saved and loaded files differ when they should be the same"
+        )
+        os.remove(BASE_DIR / "data" / "interim" / "metadata_ids.csv")
+
+    def test_filter_hf(self):
+        hf_dataset = Dataset.from_dict(
+            {"test_col": ["t"] * 5, "id": ["1", "2", "3", "4", "5"]}
+        )
+        expected = Dataset.from_dict(
+            {"test_col": ["t"] * 3, "id": ["1", "3", "5"]}
+        ).to_pandas()
+        vals_to_keep = [1, 3, 5]
+        filtered = filter_hf_dataset(hf_dataset, vals_to_keep)
+        filtered_pandas = filtered.to_pandas()
+        assert filtered_pandas.equals(
+            expected
+        ), f"""Expected and filtered datasets do not match \n\n expected: {expected}
+                                                  filtered: {filtered_pandas}"""
+
+    def test_map_hf_dataset(self):
+        ["id", "text", "source", "added", "metadata"]
+        hf_dataset = Dataset.from_dict(
+            {
+                "id": [0, 0, 1, 1],
+                "text": [
+                    "This is \n\n CHAPTER I \n\n a text with many \n\n different \xa0 paragraphs \r\n for testing",
+                    "Another \n\n I \n\n \n\n different text \xa0 paragraphs \r\n for testing \n\n small",
+                ]
+                * 2,
+                "source": ["test_source"] * 4,
+                "added": ["test_added"] * 4,
+                "metadata": [
+                    {"language": "en", "url": "test"},
+                    {"language": "fr", "url": "test"},
+                ]
+                * 2,
+            }
+        )
+        expected = Dataset.from_dict(
+            {
+                "paragraphs": [
+                    "a text with many",
+                    "different paragraphs for testing",
+                    "different text paragraphs for testing",
+                ]
+                * 2,
+                "text_ids": [0, 0, 0, 1, 1, 1],
+                "spans": [(28, 44), (50, 82), (25, 62)] * 2,
+                "chapters": ["CHAPTER I", "CHAPTER I", "I"] * 2,
+            }
+        ).to_pandas()
+        mapped_dataset = map_hf_dataset(hf_dataset).to_pandas()
+        assert mapped_dataset.equals(
+            expected
+        ), f"""Mapped and expected dataset do not match. Expected {expected}\n\n
+                                                    recovered {mapped_dataset}"""
