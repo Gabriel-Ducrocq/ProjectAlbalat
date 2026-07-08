d1ac37280475992903166a5e7e81ef8fd6703893
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2026-07-08 15:21:28 +0200

    trying to embed the chunks in a parquet file on remote

--- albalat/scripts/embed_chunks.py
+++ albalat/scripts/embed_chunks.py
@@ -6,11 +6,25 @@ It loads the model, places it on GPU.
 """
 
 import os
+
+import datasets
 import torch
 import typer
-from tqdm import tqdm
-import pyarrow.parquet as pq
+from datasets import Dataset
+from dataclasses import dataclass
 from sentence_transformers import SentenceTransformer
+from datasets.distributed import split_dataset_by_node
+
+
+@dataclass
+class encodingParameters:
+    """Class that contains all the parameters for the encoding."""
+
+    num_proc: int = 4
+    batch_size: int = 8
+    convert_to_numpy: bool = True
+    normalize_embeddings: bool = True
+    batched: bool = True
 
 
 def get_device(local_rank: int = 0) -> str:
@@ -48,7 +62,7 @@ def compute_start_end_indexes(
     return start_index, end_index
 
 
-def load_chunks_datasets(dataset_path: str) -> pq.ParquetFile:
+def load_chunks_datasets(dataset_path: str) -> Dataset:
     """
     Reads the dataset only made of the chunks we assign to that GPU.
     :param dataset_path: path to the dataset in arrow format.
@@ -57,7 +71,7 @@ def load_chunks_datasets(dataset_path: str) -> pq.ParquetFile:
     assert dataset_path.endswith((".parquet")), (
         f"""The dataset file must be an arrow file. Currently {dataset_path}."""
     )
-    chunk_dataset = pq.ParquetFile(dataset_path)
+    chunk_dataset = datasets.load_dataset("parquet", data_files=dataset_path)["train"]
     return chunk_dataset
 
 
@@ -95,24 +109,61 @@ def load_model(
     )
 
 
+def encode_chunk(
+    sample: str,
+    model: SentenceTransformer,
+    encoding_parameters: encodingParameters,
+) -> dict:
+    """
+    Encode the chunks in that batch.
+    :param model: model that we use for encoding.
+    :param sample: samples of the dataset.
+    :param batch_size: size of the batch for the encode method.
+    :param convert_to_numpy: whether to convert to numpy or not.
+    :param normalize_embeddings: whether to normalize embeddings.
+    :param num_proc: number of workers.
+    :return: a dictionnary corresponding to the embeddings output of the batch.
+    """
+    return {
+        "embeddings": model.encode(
+            sample,
+            batch_size=encoding_parameters.batch_size,
+            convert_to_numpy=encoding_parameters.convert_to_numpy,
+            normalize_embeddings=encoding_parameters.normalize_embeddings,
+            num_proc=encoding_parameters.num_proc,
+        )
+    }
+
+
 def embed(
+    output_dataset_path: str,
     dataset_path: str,
     model_name: str,
-    batch_size_arrow: int = 4096,
+    batch_size: int = 8,
     model_dtype: str = "torch.bfloat16",
     atten_implem: str = "sdpa",
-    batch_size_encoder: int = 8,
+    num_proc: int = 4,
+    convert_to_numpy: bool = True,
+    normalize_embeddings: bool = True,
+    batched: bool = True,
+    writer_batch_size=128,
 ) -> None:
     """
     Function responsible for embedding the chunks. It only embeds the chunks based on its GPU number ahd the total
     number of GPUs in the distributed training.
+    :param output_dataset_path: path where to save the dataset.
     :param dataset_path: path to the dataset containing the chunks, it must be an arrow file.
     :param model_name: name of the model that will loaded by Hugging Face.
     :param local_rank: GPU number.
-    :param batch_size_arrow: size of the batch used to read each row group in the dataset.
+    :param batch_size: size of the batch used to read each row group in the dataset.
     :param model_dtype: dtype of the embedding model, in ["torch.bfloat16", "torch.float32", "torch.float16"].
     :param atten_implem: implementation of the attention mechanism, must be in ["sdpa", "flash_attention_2"].
     :param batch_size_encoder: batch size to use in the encoding.
+    :param num_proc: number of proc to use.
+    :param convert_to_numpy: whether we should convert to numpy.
+    :param normalize_embeddings: whether we should normalize the embeddings.
+    :param batched: whether to use the batched version of the .map method.
+    :param writer_batch_size: how samples to accumulate before writing on disk.
     :return: None.
     """
 
@@ -125,28 +176,36 @@ def embed(
     ), f"""The model_name must be a string, currently
                                                                     {type(model_name)}."""
 
+    assert output_dataset_path.endswith(".parquet"), f"""
+                                        The output file be in parquet format, currently {output_dataset_path}"""
+
+    encoding_parameters = encodingParameters(
+        num_proc=num_proc,
+        batch_size=batch_size,
+        convert_to_numpy=convert_to_numpy,
+        normalize_embeddings=normalize_embeddings,
+        batched=batched,
+    )
     world_size = int(os.environ["WORLD_SIZE"])
     local_rank = int(os.environ["LOCAL_RANK"])
-    print(f"local_rank={local_rank}, world_size={world_size}")
-    chunk_dataset = load_chunks_datasets(dataset_path)
-    start_index, end_index = compute_start_end_indexes(
-        local_rank, world_size, chunk_dataset.num_row_groups
-    )
     device = get_device(local_rank)
+    print(f"local_rank={local_rank}, world_size={world_size}")
     model = load_model(model_name, device, model_dtype, atten_implem)
-    for row_group in tqdm(
-        range(start_index, end_index), desc=f"GPU:{local_rank}", position=local_rank
-    ):
-        row_group_chunks = chunk_dataset.read_row_group(
-            row_group, columns=["text_ids", "spans", "chapters", "paragraphs"]
-        )
-        for batch in row_group_chunks.to_batches(max_chunksize=batch_size_arrow):
-            texts = batch["paragraphs"].to_pylist()
-            model.encode(
-                texts,
-                batch_size=batch_size_encoder,
-                normalize_embeddings=True,
-            )
+    chunk_dataset = load_chunks_datasets(dataset_path)
+    chunk_dataset = split_dataset_by_node(
+        chunk_dataset, rank=local_rank, world_size=world_size
+    )
+    chunk_dataset = chunk_dataset.map(
+        lambda chunk, encoding_params, model_embed: encode_chunk(
+            chunk["paragraphs"], model_embed, encoding_parameters
+        ),
+        fn_kwargs={"encoding_params": encoding_parameters, "model": model},
+        batch_size=encoding_parameters.batch_size,
+        batched=encoding_parameters.batched,
+        writer_batch_size=writer_batch_size,
+    )
+
+    chunk_dataset.to_parquet(output_dataset_path)
 
 
 if __name__ == "__main__":
