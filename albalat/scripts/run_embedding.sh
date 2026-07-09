441cd4b5997554e154a84f506da7e90294537c09
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2026-07-09 15:01:19 +0200

    fixing bug in embedding script

--- albalat/scripts/run_embedding.sh
+++ albalat/scripts/run_embedding.sh
@@ -2,5 +2,5 @@
 
 N_GPUS=$SLURM_GPUS_ON_NODE
 source .venv/bin/activate
-torchrun --standalone --nnodes=1 --nproc-per-node=N_GPUS -p embedding.nproc_per_node albalat/scripts/embed_chunks.py albalat/data/processed/embeddings albalat/data/processed/chunks.parquet
+torchrun --standalone --nnodes=1 --nproc-per-node=N_GPUS albalat/scripts/embed_chunks.py albalat/data/processed/embeddings albalat/data/processed/chunks.parquet
 
