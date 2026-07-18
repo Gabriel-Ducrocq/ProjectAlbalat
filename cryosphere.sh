69cc7477049977526791dc38b55fdb29ac9d5139
Gabriel-Ducrocq <gabrielducrocq.maths@gmail.com>
2026-07-18 13:45:18 +0200

    commit before push

--- cryosphere.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --gpus=1 
-#SBATCH -A berzelius-2025-303 
-#SBATCH -t 24:00:00
-#BATCH --reservation safe
-module load Mambaforge
-#conda activate cryoSPHERE_pose_estimation
-conda activate /proj/berzelius-2025-303/users/x_gabdu/cryosphere061_dev
-cryosphere_train --experiment_yaml parameters_mcl.yaml
--- repro.sh
+++ repro.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #SBATCH --gpus=4
+source slurm_config.sh
 #SBATCH -A ${SLURM_ACCOUNT} 
 #SBATCH -t 10:00:00
+#SBATCH --reservation safe
 source .venv/bin/activate
-source slurm_config.sh
+export HF_HOME=/proj/berzelius-2025-303/users/x_gabdu/random/hf_cache
 dvc repro
